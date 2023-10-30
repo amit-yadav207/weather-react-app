c6646b4445a241105e2a922c89c57a56e7d6a0ce
amit-yadav207 <amit.yd725@gmail.com>
2023-10-31 03:10:51 +0530

    dark mode aaded and recent search history and transformation of icons added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-// App.js
+
 import React, { useState } from 'react';
 import './App.css';
 import Weather from './components/Weather';
+
+function RecentSearch({ data, region }) {
+  if (data && data.cod === 200) {
+    return (
+      <div>
+        <Weather data={data} city={region} />
+      </div>
+    );
+  } else {
+    return <p className='blank'>Enter a location to get weather information</p>;
+  }
+}
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [region, setRegion] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [recentSearches, setRecentSearches] = useState([]);
 
-  const fetchWeatherData = async () => {
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const fetchWeatherData = async (location) => {
     const API_KEY = 'd15487d4b5c1617e2ca2b36c1ce1e816';
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&appid=${API_KEY}`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
 
     try {
       const response = await fetch(URL);
@@ -32,15 +50,32 @@ function App() {
       alert('Please enter a city/country name.');
       return;
     }
-    fetchWeatherData();
-    setRegion(searchQuery.charAt(0).toUpperCase() + searchQuery.slice(1));
+    const formattedSearchQuery = searchQuery.charAt(0).toUpperCase() + searchQuery.slice(1);
+    setRegion(formattedSearchQuery);
+
+    // Check if the location is already in the recent searches
+    if (!recentSearches.includes(formattedSearchQuery)) {
+      fetchWeatherData(formattedSearchQuery); // Only fetch data if it's not in recent searches
+      setRecentSearches((prevSearches) => [formattedSearchQuery, ...prevSearches.slice(0, 3)]);
+    }
+
     setSearchQuery('');
   }
 
+  const handleRecentSearchClick = (search) => {
+    fetchWeatherData(search);
+    setRegion(search);
+  }
+
   return (
-    <div className="App">
-      <header>
+    <div className={`App ${isDarkMode ? 'dark-mode' : ''}`}>
+      <header className={`${isDarkMode ? 'dark-mode' : ''}`}>
         <h1>Weather App <i className="fas fa-cloud-sun"></i></h1>
+        {isDarkMode ? (
+          <i className="fas fa-sun" onClick={toggleDarkMode}></i>
+        ) : (
+          <i className="fas fa-moon" onClick={toggleDarkMode}></i>
+        )}
       </header>
       <div className="form-container">
         <form onSubmit={handleSubmit}>
@@ -57,15 +92,23 @@ function App() {
           </button>
         </form>
       </div>
-      {weatherData && weatherData.cod === 200 ? (
-        <div>
-          <Weather data={weatherData} city={region} />
+      {recentSearches.length > 0 && (
+        <div className="recent-searches">
+          <h2>Recent Searches:</h2>
+          <ul>
+            {recentSearches.map((search, index) => (
+              <li key={index} onClick={() => handleRecentSearchClick(search)}>
+                {search}
+              </li>
+            ))}
+          </ul>
         </div>
-      ) : (
-        <p className='blank'>Enter a location to get weather information</p>
       )}
+      <RecentSearch data={weatherData} region={region} />
     </div>
   );
 }
 
 export default App;
+
+
